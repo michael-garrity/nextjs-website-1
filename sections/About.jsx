c0d1b278b58f24a478dde184119f08db61d6d4c4
Mike Garrity <garritymichael@protonmail.com>
2022-12-06 16:47:06 -0600

    about section

--- sections/About.jsx
+++ sections/About.jsx
@@ -1,9 +1,48 @@
 'use client';
 
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const handleClickScroll = () => {
+  const element = document.getElementById('explore');
+  if (element) {
+    element.scrollIntoView({ behavior: 'smooth' });
+  }
+};
+
 const About = () => (
-  <section id="about">
-    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+
+  <section className={`${styles.paddings} relative z-10`} id="about">
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: true, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="About LoremIpsum" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+        onClick={handleClickScroll}
+      />
+    </motion.div>
   </section>
 );
 
 export default About;
+
